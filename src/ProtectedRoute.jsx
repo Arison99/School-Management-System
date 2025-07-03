64a0ee81e604ffac5818de7768c2d34a66aafa4a
Arison99 <harrisondaviinci@gmail.com>
2025-07-03 09:34:58 +0300

    Delete ProtectedRoute.jsx

--- src/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ element }) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-
-  return isAuthenticated ? element : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
