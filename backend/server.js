9818c91b9d6c653d204ac249c85ae822e7635eb6
Arison99 <harrisondaviinci@gmail.com>
2025-07-23 14:26:15 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const studentsDir = path.join(__dirname, 'uploads/students');
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL ||'http://192.168.1.2:5173',
     credentials: true
 }));
 app.use(express.json());
